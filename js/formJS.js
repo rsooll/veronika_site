14d97bafa999b5a14eaab0e6ff59d5b43c756ad2
rsooll <rsooll.social@gmail.com>
2021-11-17 21:23:43 +0300

    feat; form left 100

--- js/formJS.js
+++ js/formJS.js
@@ -1,3 +1,18 @@
+(function () {
+    const form = document.querySelector('.contactFormButton');
+    form.addEventListener('click', () => {
+        form.classList.add('contactFormButtonActive');
+    });
+}());
 
 
 
+//function checkTel() {
+//     const telNumber = document.querySelector('.tel');
+//     if (telNumber <= 13) {
+//         alert('не правильно указан номер');
+//         return false;
+//     }
+// };
+
+ 
