84c863b012a5f8d4ad38d79a50b2851e3ef2e497
rsooll <rsooll.social@gmail.com>
2021-11-24 10:34:51 +0300

    feat: 2/3 slider did

--- js/js-pages.js
+++ js/js-pages.js
@@ -5,5 +5,72 @@
     burger.classList.toggle("burger_active");
   });
 })();
-// слайлер
+// СЛАЙДЕР
 
+// пременные и константы 
+
+const previousSlide = document.querySelector('.previousSlide');
+const nextSlider = document.querySelector('.nextSlide');
+const sliderBlock = document.querySelectorAll('.sliderBlock');
+const slideDot = document.querySelectorAll('.slideDot');
+
+let index = 0;
+
+// клики по кнопкам
+
+const activeSlide = i => {
+  for (slide of sliderBlock) {
+    slide.classList.remove('sliderBlockVisible');
+  }
+  sliderBlock[i].classList.add('sliderBlockVisible');
+};
+
+
+const nextSlide = () => {
+  if (index == sliderBlock.length - 1) {
+    index = 0;
+    activeSlide(index);
+  } else {
+    index++;
+    activeSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index == 0) {
+    index = sliderBlock.length - 1;
+    activeSlide(index);
+  } else {
+    index--;
+    activeSlide(index);
+  }
+};
+nextSlider.addEventListener('click', nextSlide);
+previousSlide.addEventListener('click', prevSlide);
+
+
+// точки внизу изменение
+
+const activeDotes = i => {
+  for (dote of slideDot) {
+    dote.classList.remove('slideDotActive');
+  }
+  sliderBlock[i].classList.add('slideDotActive');
+};
+
+const ClearDots = ind => {
+  activeDotes(ind);
+};
+
+//клики по точкам
+
+slideDot.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    activeSlide(index);
+  })
+})
+
+//автоматические переходы слайда
+
+setInterval(nextSlide, 2500);
